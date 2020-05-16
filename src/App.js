77327de0f4bcbd308ac70ea311a00bbb6cd74916
aayushishere <aayushsingh2203@gmail.com>
2020-05-16 16:14:20 +0530

    uploading files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import "./stylesheet/styles.css";
 import "./stylesheet/sidebar.css";
 import "./stylesheet/navbar.css";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 
-export default function App() {
-  return (
-    <>
-      <Navbar />
-      <Sidebar />
-    </>
-  );
+class App extends Component {
+  render(){
+    return (
+      <>
+        <Navbar />
+        <Sidebar />
+      </>
+    );
+  }
 }
+
+export default App;
